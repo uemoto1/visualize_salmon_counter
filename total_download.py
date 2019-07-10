e44f14f8051457efaff097041dafd4f7454e228c
Mitsuharu Uemoto <uemoto@ccs.tsukuba.ac.jp>
2019-07-10 16:54:28 +0900

    fix: removed japanese letters

--- total_download.py
+++ total_download.py
@@ -4,10 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import re
 
-# データファイル
+# Datafile
 df = pd.read_csv("./salmon_download.csv")
 
-# ファイル名正規表現マッチング
+# Reglar expression to match the download filename
 ptn = re.compile(r"/download/SALMON-v[\.\d]*?.tar.gz")
 
 df.index = pd.to_datetime(df["Date"])
@@ -16,11 +16,11 @@ flag = df["Filetitle"].apply(lambda x: ptn.match(x) is not None)
 df["Count"] = 0
 df.loc[flag, "Count"] = 1
 
-# ダウンロード数集計処理 (2017-06-14以降)
+# Accumulating total downloads
 df_day = df.resample("1D").sum()
 df_total = df_day["2017-06-14" <= df_day.index].cumsum(axis=0)
 
-# プロット
+# Plot
 plt.figure(figsize=[7, 5])
 plt.fill_between(df_total.index, df_total["Count"],
                  facecolor="salmon", edgecolor="darkred", lw=1.5)
